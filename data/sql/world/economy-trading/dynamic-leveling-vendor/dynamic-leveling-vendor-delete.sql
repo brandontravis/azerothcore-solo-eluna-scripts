8532c5ac63b24498bb8d430ac5fd6f4d981dfcfc
Brandon Travis <btravis@prolifichq.com>
2025-06-26 17:00:35 -0400

    SQL files

--- data/sql/world/economy-trading/dynamic-leveling-vendor/dynamic-leveling-vendor-delete.sql
+++ data/sql/world/economy-trading/dynamic-leveling-vendor/dynamic-leveling-vendor-delete.sql
@@ -1,1 +1,8 @@
- 
+-- Remove Dynamic Leveling Vendor NPC
+-- Entry: 90000
+
+-- Remove any spawned instances of the vendor
+DELETE FROM `creature` WHERE `id` = 90000;
+
+-- Remove the creature template
+DELETE FROM `creature_template` WHERE `entry` = 90000; 
--- data/sql/world/economy-trading/dynamic-leveling-vendor/dynamic-leveling-vendor.sql
+++ data/sql/world/economy-trading/dynamic-leveling-vendor/dynamic-leveling-vendor.sql
@@ -1,1 +1,10 @@
- 
+-- Add Dynamic Leveling Vendor NPC
+-- Entry: 90000
+
+-- Add the creature template
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `unit_flags3`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetInhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(90000, 0, 0, 0, 0, 0, 1300, 0, 0, 0, 'Matilda Matgrind', 'Crafting Materials & Recipes', '', 0, 80, 80, 2, 35, 128, 1, 1.14286, 1, 1, 1, 0, 0, 1, 2000, 2000, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 0);
+
+-- Add the creature spawn (optional - you can spawn manually with .spawnvendor)
+-- INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnDifficulties`, `phaseUseFlags`, `PhaseId`, `PhaseGroup`, `terrainSwapMap`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `unit_flags2`, `unit_flags3`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
+-- (1, 90000, 0, 0, 0, '0', 0, 0, 0, -1, 0, 0, -8913, 554, 94, 0, 300, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, '', 0); 
